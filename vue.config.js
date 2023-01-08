c4774fe548d9b01b9c2db3ee67bb3f7ba7055e04
HiBixby <201910789@sangmyung.kr>
2023-01-08 15:20:56 +0900

    👷 Github pages 배포를 위한 설정 추가

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  outputDir: "./docs", //build 결과물을 docs라는 폴더에 저장
+  publicPath: "./",
+  assetsDir: "./",
+});
