de068ef2d0f4282ee30e168fc4fe63b97e08a846
HiBixby <201910789@sangmyung.kr>
2023-01-08 11:04:46 +0900

    ✨ Add sorting broad list func (#2)

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,11 @@
   <div class="title-wrap">
     <h2>전체</h2>
     <form>
-      <select>
+      <select v-model="orderType">
         <option value="broad_start">최신순</option>
         <option value="view_cnt">시청자순</option>
       </select>
-      <button @click.prevent="" class="refresh">
+      <button @click.prevent="getBroadList" class="refresh">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
           <!--! Font Awesome Pro 6.2.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2022 Fonticons, Inc. -->
           <path
@@ -69,29 +69,29 @@ export default {
   components: {},
   data() {
     return {
+      orderType: "broad_start",
+      pageNo: 1,
       broadcastList: null,
-      broads: [
-        {
-          broad_title: "이상호 BWC포를란 내가간다 기다려라...",
-          visit_broad_type: "0",
-          is_password: "0",
-          broad_cate_no: "00040070",
-          broad_no: "244390303",
-          user_id: "lshooooo",
-          user_nick: "BJ이상호",
-          profile_img:
-            "//profile.img.afreecatv.com/LOGO/ls/lshooooo/lshooooo.jpg?dummy=4345126843",
-          broad_thumb: "//liveimg.afreecatv.com/m/244390303",
-          broad_start: "2023-01-06 12:25:42",
-          broad_grade: "0",
-          broad_bps: "8000",
-          broad_resolution: "1920x1080",
-          total_view_cnt: "14357",
-        },
-      ],
+      broads: null,
     };
   },
   methods: {
+    getBroadList() {
+      const clientId = "e8201566692601ecee34820c9862e516";
+      this.$axios
+        .get("https://openapi.afreecatv.com/broad/list", {
+          params: {
+            client_id: clientId,
+            order_type: this.orderType,
+            page_no: this.pageNo,
+          },
+        })
+        .then((res) => {
+          this.broadcastList = res.data;
+          this.broads = res.data.broad;
+        })
+        .catch((error) => console.log(error));
+    },
     getPlayerLink(i) {
       return `https://play.afreecatv.com/${this.broads[i].user_id}/${this.broads[i].broad_no}`;
     },
@@ -112,15 +112,14 @@ export default {
       return formatedString;
     },
   },
+  computed: {},
+  watch: {
+    orderType: function () {
+      this.getBroadList();
+    },
+  },
   created() {
-    this.$axios
-      .get(
-        "https://openapi.afreecatv.com/broad/list?client_id=e8201566692601ecee34820c9862e516&order_type=view_cnt&page_no=1"
-      )
-      .then((res) => {
-        this.broadcastList = res.data;
-        this.broads = res.data.broad;
-      });
+    this.getBroadList();
   },
 };
 </script>
